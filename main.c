fbda881c0a0f813964859b9136fb5fd33d75cd23
Matin-Marzie <116279956+Matin-Marzie@users.noreply.github.com>
2023-11-17 13:42:01 +0200

    Update main.c 2
    τελείωσε ο αναλυτικός έλεγχος των παραμέτρων εισόδου!

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 // ----------Checks-if-a-file-exists----------
 int fileExists(const char *fileName)
 {
-    return 0;
+    return 1;
 }
 
 void print_usage()
@@ -20,17 +20,17 @@ void print_usage()
 }
 
 // -bxxxxx : checks if every x is digit!
-int is_str_part_digit(char* arr[], int n, size_t len)
+// @param j : it is the index of the first dimention of => argv[j][]
+int is_str_part_digit(char *arr[], int j, int n, size_t end)
 {
-    for (int i = n; i < len; i++)
+    for (int i = n; i < end; i++)
+    {
+        if (!isdigit(arr[j][i]))
         {
-            if (!isdigit(arr[i]))
-            {
-                print_usage();
-
-                exit(11);
-            }
+            return 0;
         }
+    }
+    return 1;
 }
 
 int main(int argc, char *argv[])
@@ -41,20 +41,38 @@ int main(int argc, char *argv[])
     int chars_read, chars_write;
     char buf[512];
 
+    size_t argv_1_len = strlen(argv[1]);
+
     // ----------Checking-the-arguments----------
+    // If user haven't give any parameter
     if (argc <= 1)
     {
         print_usage();
         exit(1);
     }
+    else if (argc > 4)
+    {
+        // If user puts space between -b and buffer size, ex. mycopy -b 1024 file1 file2
+        if (argv_1_len == 2 && is_str_part_digit(argv, 2, 0, strlen(argv[2])))
+        {
+            printf("Incorrect form of buffer specification!\n");
+            printf("example: [-b1024]\n\n");
+            print_usage();
+            exit(17);
+        }
+        else
+        {
+            printf("Too many parameters!\n");
+            print_usage();
+            exit(18);
+        }
+    }
     // if argv[1] has the format of '-bxxxxx'
     else if (argv[1][0] == '-' && argv[1][1] == 'b')
     {
-        size_t argv_1_len = strlen(argv[1]);
-        // If user puts space between -b and buffer size, ex. mycopy -b 1024 file1 file2
-        // Or
+        
         // If user doesn't specify the buffer size!
-        if (argv_1_len = 2)
+        if (argv_1_len == 2)
         {
             printf("Din't specify the buffer size!\n");
             printf("example: [-b1024]\n\n");
@@ -70,76 +88,86 @@ int main(int argc, char *argv[])
             exit(13);
         }
         // Checks if every x of -bxxxxx is int
-        else if(is_str_part_digit(argv[1], 2, argv_1_len))
+        else if (!is_str_part_digit(argv, 1, 2, argv_1_len))
         {
-            printf("Buffer size is not valid!, it must be integer!");
+            printf("Buffer size is not valid!, can be only integer!\n");
             print_usage();
             exit(14);
         }
         else
         {
-            printf("successes! 23");
+            if (!fileExists(argv[2]))
+            {
+             printf("\"%s\" does not exist!", argv[2]);
+             exit(19);
+            }
+            printf("starting the proccess of copying with buffer read() write()");
+            // ...open()
+            
         }
-
-        
     }
     else
     {
-        printf("not -b");
+        if (!fileExists(argv[1]))
+            {
+             printf("\"%s\" does not exist!", argv[1]);
+             exit(19);
+            }
+        // ...fopen()
+        
     }
 
     printf("\"%s\" not found!\n", argv[1]);
     exit(8);
 
+    // ----------Openning-the-files----------
+    if ((file1 = open("tmp.txt", O_RDONLY)) < 0)
+    {
+        perror("Open1: ");
+        exit(1);
+    }
 
-// ----------Openning-the-files----------
-if ((file1 = open("tmp.txt", O_RDONLY)) < 0)
-{
-    perror("Open1: ");
-    exit(1);
-}
+    if ((file2 = open("tmp2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600) < 0))
+    {
+        perror("Open2: ");
+        exit(5);
+    }
 
-if ((file2 = open("tmp2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600) < 0))
-{
-    perror("Open2: ");
-    exit(5);
-}
-
-chars_read = read(file1, buf, sizeof(buf));
-
-while (chars_read > 0)
-{
     chars_read = read(file1, buf, sizeof(buf));
+
+    while (chars_read > 0)
+    {
+        chars_read = read(file1, buf, sizeof(buf));
+        if (chars_read < 0)
+        {
+            perror("read: ");
+            exit(2);
+        }
+
+        chars_write = write(file2, buf, chars_read);
+        if (chars_write < 0)
+        {
+            perror("write: ");
+            exit(7);
+        }
+    }
     if (chars_read < 0)
     {
-        perror("read: ");
-        exit(2);
+        perror("reading error: ");
+        exit(3);
     }
 
-    chars_write = write(file2, buf, chars_read);
-    if (chars_write < 0)
+    if (close(file1) < 0)
     {
-        perror("write: ");
-        exit(7);
+        perror("close1: ");
+        exit(4);
     }
-}
-if (chars_read < 0)
-{
-    perror("reading error: ");
-    exit(3);
-}
 
-if (close(file1) < 0)
-{
-    perror("close1: ");
-    exit(4);
-}
+    if (close(file2) < 0)
+    {
+        perror("close2: ");
+        exit(4);
+    }
 
-if (close(file2) < 0)
-{
-    perror("close2: ");
-    exit(4);
-}
-
-return 0;
+    return 0;
 }
