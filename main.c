61d340b26d7c2a68a9f1a2524f190529fdfc8787
Matin-Marzie <116279956+Matin-Marzie@users.noreply.github.com>
2023-11-18 19:04:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include <sys/stat.h>
 #include <time.h>
 
-
 struct stat fileStat1, fileStat2;
 
 // ----------Checks-if-a-file-exists----------
@@ -48,11 +47,12 @@ int main(int argc, char *argv[])
 
     // ----------Variable-decleration----------
     int file1, file2;
+    int using_standard_buffer = 1;
+    int buffer_size;
     int chars_read, chars_write;
     clock_t start, elapsed;
     double time_spent;
 
-    size_t argv_1_len = strlen(argv[1]);
 
     // ----------Checking-the-arguments----------
     // If user haven't give any parameter
@@ -63,6 +63,7 @@ int main(int argc, char *argv[])
     }
     else if (argc > 4)
     {
+         size_t argv_1_len = strlen(argv[1]);
         // If user puts space between -b and buffer size, ex. mycopy -b 1024 file1 file2
         if (argv_1_len == 2 && is_str_part_digit(argv, 2, 0, strlen(argv[2])))
         {
@@ -81,6 +82,7 @@ int main(int argc, char *argv[])
     // if argv[1] has the format of '-bxxxxx'
     else if (argv[1][0] == '-' && argv[1][1] == 'b')
     {
+        size_t argv_1_len = strlen(argv[1]);
         // If user doesn't specify the buffer size!
         if (argv_1_len == 2)
         {
@@ -120,24 +122,25 @@ int main(int argc, char *argv[])
             }
             if (argc == 3)
             {
-                printf("didn't specify the destination file!\n");
+                printf("Didn't specify the destination file!\n");
                 print_usage();
                 exit(23);
             }
 
             // we take the integer part of '-b1024'
             char *temp_string = argv[1] + 2;
-            int buffer_size = atoi(temp_string);
+            buffer_size = atoi(temp_string);
 
             // The buffer size can't be 0
             if (buffer_size == 0)
             {
                 printf("The buffer size can't be 0\n");
-                printf("example: -b1024\n");
+                printf("example: [-b1024]\n");
                 print_usage();
                 exit(21);
             }
             char buf[buffer_size];
+            int buffer_size = 0;
 
             // ----------Openning-the-files----------
             if ((file1 = open(argv[2], O_RDONLY)) < 0)
@@ -145,7 +148,8 @@ int main(int argc, char *argv[])
                 perror("Open1: ");
                 exit(1);
             }
-            if(stat(argv[2], &fileStat1) != 0) exit(26);
+            if (stat(argv[2], &fileStat1) != 0)
+                exit(26);
 
             if ((file2 = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0600)) < 0)
             {
@@ -191,13 +195,21 @@ int main(int argc, char *argv[])
                 exit(4);
             }
 
-            if(stat(argv[3], &fileStat2) != 0) exit(27);
             if (close(file2) < 0)
             {
                 perror("close2: ");
                 exit(4);
             }
+            if (stat(argv[3], &fileStat2) != 0)
+                exit(27);
         }
+    }
+    // if user wants to use standard buffer can't insert more than 3 parameters
+    else if(argc == 4)
+    {
+        printf("Too many parameters!\n");
+        print_usage();
+        exit(40);
     }
     else
     {
@@ -206,13 +218,13 @@ int main(int argc, char *argv[])
             printf("\"%s\" does not exist!\n", argv[1]);
             exit(19);
         }
-        else if(argc == 2)
+        else if (argc == 2)
         {
             printf("Didn't specify the destination file!\n");
             print_usage();
             exit(24);
         }
-        else if(argc == 4)
+        else if (argc == 4)
         {
             printf("Invalid Command!\nToo many parameters!\n");
             print_usage();
@@ -228,7 +240,8 @@ int main(int argc, char *argv[])
                 perror("Σφάλμα κατά το άνοιγμα του πρώτου αρχείου. \n");
                 return 1;
             }
-            if(stat(argv[1], &fileStat1) != 0) exit(29);
+            if (stat(argv[1], &fileStat1) != 0)
+                exit(29);
 
             FILE *file2 = fopen(argv[2], "w");
             if (file2 == NULL)
@@ -256,19 +269,42 @@ int main(int argc, char *argv[])
             // time_end
             elapsed = clock() - start;
 
-            if(stat(argv[2], &fileStat2) != 0) exit(30);
             fclose(file1);
             fclose(file2);
+            if (stat(argv[2], &fileStat2) != 0)
+                exit(30);
         }
     }
 
 
-    // Reporting ...
-    time_spent = (double)elapsed * 1000.0 / CLOCKS_PER_SEC;
 
-    printf("time spent: %lf\n", time_spent);
-    printf("file1 size: %lld\n", (long long)fileStat1.st_size);
-    printf("file2 size: %lld\n", (long long)fileStat2.st_size);
+    // ---------------------------Reporting ...  ---------------------------
+    time_spent = (double)elapsed / CLOCKS_PER_SEC;
+
+    off_t file_2_size = fileStat2.st_size;
+
+    // velocity KB/s  (kiloBytes / second)
+    long double copy_velocity = file_2_size / time_spent;
+
+    // Writing on report.out file
+    FILE *report_file;
+    if ((report_file = fopen("report.out", "a+")) == NULL)
+    {
+        perror("Error while opening the report.out file!\n");
+        exit(32);
+    }
+
+    const char* using = "STANDARD BUFFER";
+    if(using_standard_buffer)
+    {
+        fprintf(report_file, "%-20s%-10d%-25Lf\n", using, buffer_size, copy_velocity);
+    }
+
+    if (fclose(report_file) != 0)
+    {
+        perror("closing report.out: ");
+        exit(35);
+    }
 
     return 0;
 }
